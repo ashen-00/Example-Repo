8647eb0789ab78d856eb4a49c40771910d546b10
asoo <61583371+ashen-00@users.noreply.github.com>
2023-05-24 14:50:45 -0500

    Create main.py
    Take user input and either roll a 6 sided dice or quit the program

---
+++ main.py
@@ -0,0 +1,11 @@
+import random 
+
+if __name__ == "__main__":
+   while True:     
+      print(''' 1. roll the dice             2. exit     ''')    
+      user = int(input("what you want to do\n"))     
+      if user==1:         
+         number = random.randint(1,6)         
+         print(number)     
+      else:         
+         break
